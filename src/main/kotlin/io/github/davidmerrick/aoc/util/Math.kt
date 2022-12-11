ce972a1c39ae9be6fb29a7d7e7fc506d81b23f95
David Merrick <david@david-merrick.com>
2022-12-11 11:58:59 -0800

    Added lcm and lcd methods

--- src/main/kotlin/io/github/davidmerrick/aoc/util/Math.kt
+++ src/main/kotlin/io/github/davidmerrick/aoc/util/Math.kt
@@ -6,5 +6,44 @@ fun Pair<Int, Int>.manhattanDistanceTo(other: Pair<Int, Int>): Int {
     return abs(this.first - other.first) + abs(this.second - other.second)
 }
 
-fun Collection<Int>.product() = this.reduce { a, b -> a * b }
-fun Sequence<Int>.product() = this.reduce { a, b -> a * b }
+fun Collection<Int>.product() = this.fold(1, Int::safeMultiply)
+fun Sequence<Int>.product() = this.fold(1, Int::safeMultiply)
+fun Collection<Long>.product() = this.fold(1L, Long::safeMultiply)
+fun Sequence<Long>.product() = this.fold(1L, Long::safeMultiply)
+fun Long.safeMultiply(other: Long): Long {
+    require(other != 0L)
+    return this * other
+}
+
+fun Int.safeMultiply(other: Int): Int {
+    require(other != 0)
+    return this * other
+}
+
+/**
+ * Finds least common denominator greater than 1 in the collection
+ * null otherwise
+ */
+fun Collection<Int>.lcd(): Int? {
+    require(this.all { it > 0 })
+    var i = 2
+    while (i < this.min()) {
+        if (this.all { it % i == 0 }) return i
+        i++
+    }
+    return null
+}
+
+/**
+ * Finds least common multiple greater than 1 in the collection
+ * null otherwise
+ */
+fun Collection<Int>.lcm(): Int {
+    require(this.all { it > 0 })
+    val max = this.product()
+
+    for (i in this.min() until max) {
+        if (this.all { i % it == 0 }) return i
+    }
+    return max
+}

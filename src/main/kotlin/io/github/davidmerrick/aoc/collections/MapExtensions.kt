7bbc07d542a71274850d35a43cf0f5eeb738de16
David Merrick <david@david-merrick.com>
2023-01-01 15:59:59 -0800

    Added map extensions

--- src/main/kotlin/io/github/davidmerrick/aoc/collections/MapExtensions.kt
+++ src/main/kotlin/io/github/davidmerrick/aoc/collections/MapExtensions.kt
@@ -1,3 +1,10 @@
 package io.github.davidmerrick.aoc.collections
 
 fun <K, V> Map<K, out V>.invert() = this.map { it.value to it.key }.toMap()
+
+/**
+ * For setting values in maps that contain other maps
+ */
+operator fun <A, B, C> Map<A, MutableMap<B, C>>.set(key1: A, key2: B, value: C) {
+    getValue(key1)[key2] = value
+}
